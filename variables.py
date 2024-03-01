5ef86a3859e56fbdaadff7aaa859d824bd68d27a
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2024-03-01 12:44:06 +0100

    type and casting

--- variables.py
+++ variables.py
@@ -17,3 +17,16 @@ xx = "Sally"    # xx adesso è di tipo str
 print(xx)
 
 # per specificare il tipo di variabile si vuole lo si può fare con il casting 
+
+x = str(3)    # x sarà '3'
+y = int(3)    # y sarà 3
+z = float(3)  # z sarà 3.0
+
+print(x, y, z)
+
+# si può ottenere il tipo di variabile attraverso la funzione type()
+
+x = 5
+y = "John"
+
+print(type(x), type(y))
