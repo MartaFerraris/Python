2118c552d5a62f5e1ce73f0d6cf62af1bf23fb3c
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2024-03-01 12:31:06 +0100

    python study

---
+++ hello.py
@@ -0,0 +1,45 @@
+# come fare dei dadi random: 
+
+# import numpy as np
+
+# msg = "Roll a dice!"
+
+# print(msg)
+
+# print(np.random.randint(1,9))
+
+#--------------------------------------------------
+
+# stampare "Hello, World!" su console
+
+print("Hello, World!")
+
+#--------------------------------------------------
+
+# PYTHON INDENTATION:
+# Serve per identificare un blocco di codice, di fatti è molto importante il suo utilizzo. Comunemente utilizzano dal 1 ai 4 spazi per l'indentazione ed è buona pratica usare il medesimo spazio per tutto il blocco,
+# se no Python lancia un errore di sintassi.
+
+if 5 > 2:
+    print("Five is greater than two!")
+
+# if 5 > 2:
+#     print("Five is greater than two!")
+#         print("Five is greater than two!") [ERRORE!]
+    
+#--------------------------------------------------
+
+# PYTHON VARIABLES:
+# Python non ha comandi per dichiarare una variabile.
+    
+x = 5
+y = "Hello, World!"
+
+#--------------------------------------------------
+
+"""
+PYTHON COMMENT:
+per i commenti si usa cancelletto (#) o la shortcut Ctrl+/
+per il commento multilinea si utilizzano le triple vigolette: 
+""" 
+
---
+++ variables.py
@@ -0,0 +1,19 @@
+# VARIABILI DI PYTHON
+
+# Creare una variabile
+# Si crea una variabile nel momento in cui si assegna un valore ad essa.
+
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+# le variabili non hanno un tipo e glielo si può cambiare risettandole
+
+xx = 4          # xx è di tipo int 
+xx = "Sally"    # xx adesso è di tipo str
+
+print(xx)
+
+# per specificare il tipo di variabile si vuole lo si può fare con il casting 
