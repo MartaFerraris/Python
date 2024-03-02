878e28f81c8b8d8b20044c29466a1bba9d32ff11
Marta <marta.ferraris25@gmail.com>
2024-03-02 21:58:45 +0100

    data types

--- data_types.py
+++ data_types.py
@@ -25,7 +25,7 @@ x = 20                                      # -> <class 'int'>
 x = 20.5                                    # -> <class 'float'>
 x = 1j                                      # -> <class 'complex'>
 x = ['red', 'blue', 'green']                # -> <class 'list'>
-x = ('red', 'blue', 'green')                # -> <class 'tulpe'>
+x = ('red', 'blue', 'green')                # -> <class 'tuple'>
 x = range(4)                                # -> <class 'range'>
 x = {'name': 'Jerry', 'animal': 'mouse'}    # -> <class 'dict'>
 x = {'red', 'blue', 'green'}                # -> <class 'set'>
@@ -37,3 +37,22 @@ x = memoryview(bytes(3))                    # -> <class 'memoryview'>
 x = None                                    # -> <class 'NoneType'>
 
 print(type(x))
+
+# Setting specific data type:
+
+y = str('Hello World!')                     # -> <class 'str'>
+y = int(20)                                 # -> <class 'int'>
+y = float(20.5)                             # -> <class 'float'>
+y = complex(1j)                             # -> <class 'complex'>
+y = list(('red', 'blue', 'green'))          # -> <class 'list'>
+y = tuple(('red', 'blue', 'green'))         # -> <class 'tuple'>
+y = range(4)                                # -> <class 'range'>
+y = dict(name='Jerry', animal='mouse')      # -> <class 'dict'>
+y = set(('red', 'blue', 'green'))           # -> <class 'set'>
+y = frozenset(('red', 'blue', 'green'))     # -> <class 'frozenset'>
+y = bool(4)                                 # -> <class 'bool'>
+y = bytes(4)                                # -> <class 'bytes'>
+y = bytearray(4)                            # -> <class 'bytearray'>
+y = memoryview(bytes(4))                    # -> <class 'memoryview'>
+
+print(type(y))
